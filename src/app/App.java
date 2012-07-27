3a2728f0972cd54eb33ddf14ae4ace0bcdfb6ae6
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2012-07-27 22:50:59 +0200

    slick setup

---
+++ src/app/App.java
@@ -0,0 +1,22 @@
+package app;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+public class App {
+
+    /**
+     * @param args
+     * @throws SlickException
+     */
+    public static void main(String[] args) {
+        try {
+            AppGameContainer app = new AppGameContainer(new Game("Train"));
+            app.setDisplayMode(800, 600, false);
+            app.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/app/Game.java
@@ -0,0 +1,29 @@
+package app;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Game extends BasicGame {
+
+    public Game(String title) {
+        super(title);
+    }
+
+    @Override
+    public void render(GameContainer arg0, Graphics arg1) throws SlickException {
+
+    }
+
+    @Override
+    public void init(GameContainer arg0) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer arg0, int arg1) throws SlickException {
+
+    }
+
+}
