c303abd3dce4757631f145af9d0af62155d3cb35
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2012-10-07 00:39:25 +0200

    FontFactory rewritten to FlyWeight pattern

--- src/factory/FontFactory.java
+++ src/factory/FontFactory.java
@@ -1,5 +1,8 @@
 package factory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.newdawn.slick.Font;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.UnicodeFont;
@@ -8,9 +11,10 @@ import org.newdawn.slick.font.effects.Effect;
 public class FontFactory {
 
     private static FontFactory factory;
+    Map<String, Map<Integer, Map<Effect, Font>>> type_size_effectFont;
 
     private FontFactory() {
-
+        this.type_size_effectFont = new HashMap<String, Map<Integer, Map<Effect, Font>>>();
     }
 
     public static FontFactory getInstance() {
@@ -21,6 +25,11 @@ public class FontFactory {
     }
 
     public Font getFont(String path, String type, int size, Effect effect) throws SlickException {
+        if (this.type_size_effectFont.containsKey(type)
+                && this.type_size_effectFont.get(type).containsKey(size)
+                && this.type_size_effectFont.get(type).get(size).containsKey(effect)) {
+            return this.type_size_effectFont.get(type).get(size).get(effect);
+        }
         UnicodeFont font = new UnicodeFont(path, size, false, false);
         font.addGlyphs(32, 800);
         font.getEffects().add(effect);
