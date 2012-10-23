5e0ccfb7ce4e1aa5d55c740c1a2ba1f3a25fefd0
Miloslav Nenadal <miloslav.nenadal@imatic.cz>
2012-10-23 20:48:54 +0200

    Code conventions

--- src/other/LevelPackage.java
+++ src/other/LevelPackage.java
@@ -6,21 +6,24 @@ public class LevelPackage {
 
     private String name;
     private ArrayList<String> levelNames;
-    
-    public LevelPackage(String name, ArrayList<String> levelNames){
+
+    public LevelPackage(String name, ArrayList<String> levelNames) {
         this.name = name;
         this.levelNames = levelNames;
     }
-    
+
     public String getName() {
-        return name;
+        return this.name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public ArrayList<String> getLevelNames() {
-        return levelNames;
+        return this.levelNames;
     }
+
     public void setLevelNames(ArrayList<String> levelNames) {
         this.levelNames = levelNames;
     }
