2920104c0e8f6d74235d75a7adf6b4c20a4d0c73
Jiří Bobrek <bobrekjiri@gmail.com>
2013-01-13 19:25:36 +0100

    Updated MenuItem
    Added Colors and IsEnabled properties.

--- src/entity/MenuItem.java
+++ src/entity/MenuItem.java
@@ -2,9 +2,13 @@ package entity;
 
 import java.awt.event.ActionListener;
 
+import org.newdawn.slick.Color;
+
 public class MenuItem extends Child {
 
+    private boolean isEnabled;
     private ActionListener listener;
+    private Color active, disabled;
 
     public MenuItem(String text, ActionListener listener) {
         this.setText(text);
@@ -13,6 +17,20 @@ public class MenuItem extends Child {
 
     public ActionListener getListener() {
         return this.listener;
+    }
+
+    public void setColors(Color normal, Color active, Color disabled) {
+        super.setColor(normal);
+        this.active = active;
+        this.disabled = disabled;
+    }
+
+    public Color getActiveColor() {
+        return this.active;
+    }
+
+    public Color getDisabledColor() {
+        return this.disabled;
     }
 
     @Override
@@ -24,5 +42,13 @@ public class MenuItem extends Child {
     public int getHeight() {
         return this.getFont().getHeight(this.getText()) + this.getPaddingBottom()
                 + this.getPaddingTop();
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean isEnabled) {
+        this.isEnabled = isEnabled;
     }
 }
